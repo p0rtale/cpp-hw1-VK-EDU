fdc2e90ac33cf4f12cc580e5b992d932f0fbfdd2
Maxkashh <kashurin2001@mail.ru>
2023-01-23 23:47:55 +0300

    end

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/maxim/Технопарк/check_rewiew/cpp-hw1-VK-EDU
-# Build directory: /home/maxim/Технопарк/check_rewiew/cpp-hw1-VK-EDU/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("ustring")
-subdirs("exe")
-subdirs("tests")
--- cmake-build-debug/exe/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/maxim/Технопарк/check_rewiew/cpp-hw1-VK-EDU/exe
-# Build directory: /home/maxim/Технопарк/check_rewiew/cpp-hw1-VK-EDU/cmake-build-debug/exe
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
